4d5861bce8d02ac75fa8703b04a17f973b328532
710dom <80789907+710dom@users.noreply.github.com>
2021-03-25 21:52:50 +0100

    tache 2 cinema

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,18 +1,28 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        System.out.print("  ");
-        for (int i = 1; i < 9; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 1; i < 8; i++) {
-            System.out.print(i);
-            for (int j = 0; j < 8; j++) {
-                System.out.print(" S");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter the number of rows:");
+            int nbrRow = scanner.nextInt();
+            System.out.println("Enter the number of seats in each row:");
+            int nbrSeat = scanner.nextInt();
+            if (nbrRow * nbrSeat <= 60) {
+                System.out.println("Total income:");
+                System.out.println("$" + nbrRow * nbrSeat * 10);
+            } else {
+                if (nbrRow % 2 == 0) {
+                    System.out.println("Total income:");
+                    System.out.println("$" + (nbrRow / 2) * nbrSeat * 18);
+                } else {
+                    int nbrRowSup = nbrRow / 2;
+                    int nbrRowInf = nbrRow - nbrRowSup;
+                    System.out.println("Total income:");
+                    System.out.println("$" + nbrSeat * (nbrRowSup * 10 + nbrRowInf * 8));
+                }
             }
         }
     }
